bd77a54a530866457605227a99b1d1ddaa282ab1
agata okolska <agata.okolska@gmail.com>
2018-08-21 13:06:40 +0200

    test with value 0

--- src/orderTotal.js
+++ src/orderTotal.js
@@ -1,11 +1,22 @@
-export const orderTotal = (cart) => (
-    cart.reduce((r, el, i, arr) =>
-        r += el.price * (el.quantity || 1)
-        , 0)
-)
+// export const orderTotal = (cart) => (
+//     cart.reduce((r, el, i, arr) =>
+//         r += el.price * (el.quantity || 1)
+//         , 0)
+// )
 
 // export const orderTotal = (cart) => (
 //     cart.reduce((r, el, i, arr) =>
 //         r += el.price * (el.quantity ? el.quantity : 1)
 //         , 0)
 // )
+
+export const orderTotal = (cart) => (
+    cart.reduce(
+        (r, el) => {
+            const realQuantity = el.quantity === undefined ? 1 : el.quantity
+
+            return r + (el.price * realQuantity)
+        },
+        0
+    )
+)
--- src/orderTotal.test.js
+++ src/orderTotal.test.js
@@ -12,6 +12,12 @@ const cart2 = [
     { name: 'owoc', price: 25, quantity: 1 },
     { name: 'bułka', price: 15, quantity: 2 },
 ]
+
+const cart3 = [
+    { name: 'owoc', price: 25, quantity: 0 },
+    { name: 'bułka', price: 15, quantity: 2 },
+]
+
 //test in JEST
 test('can calculate order total', () => {
     expect(orderTotal(cart0)).toBe(0)
@@ -20,6 +26,10 @@ test('can calculate empty order total', () => {
     expect(orderTotal(cart1)).toBe(45.5)
 })
 
-test('can calculate order total with quantity', () => {
+test('can calculate order total with quantity 0', () => {
     expect(orderTotal(cart2)).toBe(55)
+})
+
+test('can calculate order total with quantity', () => {
+    expect(orderTotal(cart3)).toBe(30)
 })
