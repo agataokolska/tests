e55f4965b831373d31ea87f1402f3193a29e8482
agata okolska <agata.okolska@gmail.com>
2018-08-21 12:36:49 +0200

    testing in JEST

--- src/orderTotal.test.js
+++ src/orderTotal.test.js
@@ -1,16 +1,27 @@
-const cart = [
-    {name:'owoc', price: 25},
-    {name:'bułka', price: 15},
-    {name:'pomidor', price: 5},
-    {name:'sok', price: 0.5}
+import { orderTotal } from './orderTotal'
+
+const cart0 = []
+
+const cart1 = [
+    { name: 'owoc', price: 25 },
+    { name: 'bułka', price: 15 },
+    { name: 'pomidor', price: 5 },
+    { name: 'sok', price: 0.5 }
 ]
 
 //total 45,5
 
-
-
-
 //test
-if(orderTotal(cart) !== 45.5) {
+if (orderTotal(cart1) !== 45.5) {
     throw new Error('cant calculate order')
 }
+
+//test in JEST
+
+test('can calculate order total', () => {
+    expect(orderTotal(cart1)).toBe(45.5)
+})
+
+test('can calculate order total', () => {
+    expect(orderTotal(cart0)).toBe(0)
+})
